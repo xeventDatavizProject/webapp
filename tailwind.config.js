392633f9344279190cc89e286c93a28c677bdb6e
Merith <merithndolo@gmail.com>
2022-07-20 10:14:46 +0200

    chore: improve set up files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,tsx}", "./components/**/*.{js,ts,tsx}"],
+  content: ['./pages/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["futura-pt", ...defaultTheme.fontFamily.sans],
-        playfairDisplay: [
-          '"Playfair Display"',
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ['futura-pt', ...defaultTheme.fontFamily.sans],
+        playfairDisplay: ['"Playfair Display"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         blue: {
-          primary: "#1e1c28",
-          secondary: "#282534",
-          tertiary: "#2e2b3c",
-          quaternary: "#3e3b4e",
-          quinary: "#565364",
+          primary: '#007EFF',
         },
-        gold: {
-          primary: "#996a4c",
-          secondary: "#c2a694",
+        blackish: {
+          primary: '#2F303A',
         },
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/assets/images/bg.png')",
       },
     },
   },
@@ -35,22 +30,22 @@ module.exports = {
   plugins: [
     function ({ addComponents }) {
       addComponents({
-        ".container": {
-          maxWidth: "85rem",
-          marginRight: "auto",
-          marginLeft: "auto",
-          paddingRight: "1rem",
-          paddingLeft: "1rem",
-          "@screen md": {
-            paddingRight: "2rem",
-            paddingLeft: "2rem",
+        '.container': {
+          maxWidth: '85rem',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          paddingRight: '1rem',
+          paddingLeft: '1rem',
+          '@screen md': {
+            paddingRight: '2rem',
+            paddingLeft: '2rem',
           },
         },
       });
     },
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
   ],
 };
