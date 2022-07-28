080ea8ffc634fffa4838c8713b88d913811c0ffb
Joel <joel.pokam@hetic.net>
2022-07-28 10:31:54 +0200

    update

--- components/graph/index.tsx
+++ components/graph/index.tsx
@@ -8,7 +8,7 @@ import {
   LineElement,
   PointElement,
   Title,
-  Tooltip,
+  Tooltip
 } from 'chart.js';
 import { FC, useState } from 'react';
 import { Bar, Chart, Doughnut, Line, Pie } from 'react-chartjs-2';
@@ -16,14 +16,23 @@ import { Bar, Chart, Doughnut, Line, Pie } from 'react-chartjs-2';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement, ArcElement);
 
 export const options = {
-  responsive: true,
   plugins: {
-    legend: {
-      position: 'top' as const,
-    },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart',
+      text: 'Chart.js Bar Chart - Stacked',
+    },
+  },
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
     },
   },
 };
@@ -34,7 +43,7 @@ export const data = {
     {
       label: 'Dataset 1',
       //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      data: [20, 10, 20, 10],
+      data: [20, 10, 20, 40, 35],
       test: 'OK',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
@@ -46,7 +55,7 @@ export const data = {
   ],
 };
 
-type graphTypes = 'chart' | 'pie' | 'bar' | 'line' | 'doughtnut';
+type graphTypes = 'chart' | 'pie' | 'bar' | 'line' | 'doughnut';
 
 type allGraphTypes = {
   [key in graphTypes]: JSX.Element;
@@ -55,23 +64,23 @@ type allGraphTypes = {
 const Graph: FC = () => {
   const [currentGraph, setCurrentGraph] = useState<graphTypes>('chart');
   const allGraph: allGraphTypes = {
-    chart: <Chart data={data} type='bar' />,
-    pie: <Pie data={data} />,
-    bar: <Bar data={data} />,
-    line: <Line data={data} />,
-    doughtnut: <Doughnut data={data} />,
+    chart: <Chart className='bg-white p-6 rounded-lg' options={options} data={data} type='bar'/>,
+    pie: <Pie options={options} data={data}/>,
+    bar: <Bar className='bg-white p-6 rounded-lg' options={options} data={data} />,
+    line: <Line className='bg-white p-6 rounded-lg' options={options} data={data} />,
+    doughnut: <Doughnut options={options} data={data}/>,
   };
 
   return (
-    <section className='container w-full flex'>
-      <ul className='bg-blackish-primary px-4 py-8'>
-        <li onClick={() => setCurrentGraph('chart')}>Chart</li>
-        <li onClick={() => setCurrentGraph('pie')}>Pie</li>
-        <li onClick={() => setCurrentGraph('bar')}>Bar</li>
+    <section className='container w-full flex '>
+      <ul  style={{cursor:'pointer'}} className='bg-blackish-primary px-4 py-8'>
+        <li  onClick={() => setCurrentGraph('chart')}>Chart</li>
+        <li  onClick={() => setCurrentGraph('pie')}>Pie</li>
+        <li  onClick={() => setCurrentGraph('bar')}>Bar</li>
         <li onClick={() => setCurrentGraph('line')}>Line</li>
-        <li onClick={() => setCurrentGraph('doughtnut')}>Doughtnut</li>
+        <li onClick={() => setCurrentGraph('doughnut')}>Doughnut</li>
       </ul>
-      <div className='ml-24'>{allGraph[currentGraph]}</div>
+      <div className='ml-24' style={{width:600}}>{allGraph[currentGraph]}</div>
     </section>
   );
 };
