bf5afcd8b57262c0ea2cc521128d44db4ac88f3d
Merith <merithndolo@gmail.com>
2022-10-03 17:53:40 +0200

    feat: update chart checked

--- components/graph/Donuts.tsx
+++ components/graph/Donuts.tsx
@@ -1,16 +1,8 @@
 import { BarElement, CategoryScale, Chart as ChartJS, ChartData, Legend, LinearScale, Title, Tooltip } from 'chart.js';
 import Input from 'components/common/Input/Input';
-import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
-import REQUESTDATA from './mock-request-list';
-
-
-interface performancesListProps {
-  data: boolean;
-  labels: boolean;
-  setSelect: Dispatch<SetStateAction<any>>;
-
-}
+import Logs from './mock-request-list';
 
 const options = {
   plugins: {
@@ -20,80 +12,88 @@ const options = {
   },
 };
 
-const Donuts: FC<performancesListProps> = ({setSelect}) => {
-
+const Donuts: FC = () => {
   ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-  const [requestData, setData] = useState<any>()
-  const [selected, setSelected] = useState<boolean>(true)
-  const selectQuery =(checkTypeByUser:string[])=>{
-     const query:[] = [];
-     REQUESTDATA.forEach(element=>{
-       checkTypeByUser.forEach( type=>{
-        if (element.query.includes(type)){
-          query.push(element);
-       }
-       })
-     })
-     return query
-  }
- const list: string[] = []
-let selectList: any[];
-let data: number[] = [];
-let labels: string[] = [];
+  const list: string[] = [];
+  let selectList: any[];
+  let data: number[] = [];
+  let labels: string[] = [];
+  const query: any[] = [];
+  const [selected, setSelected] = useState<boolean>(true);
+  const [checkedValue, setCheckedValue] = useState([]);
 
-const generateChartData = (): ChartData<"bar"> => {
-  list.push("select")
-  selectList = selectQuery(list)
-  selectList.map((x) => {
-    data.push(x.times_called);
-    labels.push(x.query)
-  })
-  return {
-    labels,
-    datasets: [
-      {
-        label: "New Confirmed",
-        data,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-};
+  const selectQuery = (queries: string[]) => {
+    Logs.forEach(log => {
+      queries.forEach(type => {
+        if (log.query.includes(type)) {
+          query.push(log);
+        }
+      });
+    });
+    return query;
+  };
 
-};
-useEffect(()=>{
-  return setSelected(<Bar data={generateChartData()} options={options} />);
-},[selected]);
+  // const CheckHandler = (e: React.FormEvent<HTMLInputElement>) => {
+  //   const value = e.currentTarget.value;
+  //   setCheckedValue((prev: any) =>
+  //   checkedValue.includes(value)
+  //       ? prev.filter((cur : string) => cur !== value)
+  //       : [...prev, e.currentTarget.value]
+  //   );
+  // };
+
+  const generateChartData = (): ChartData<'bar'> => {
+    if (selected) list.push('select');
+    else list.filter(e => e !== 'select');
+
+    selectList = selectQuery(list);
+    selectList.map(x => {
+      data.push(x.times_called);
+      labels.push(x.query);
+    });
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: 'New Confirmed',
+          data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
 
   return (
     <div>
-     <div className='w-56 mx-1 h-1/3 mt-1 shadow-lg rounded-lg p-4 z-[99999999]   border-zinc-800'>
-            <div className='flex-grow z-20  font-mono tracking-widest text-xs mb-5'>hide/show Query types</div>
-            <Input.Checkbox
-                label='select'
-                id='select-toggle'
-                value='select'
-                className='peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 peer-checked:bg-blue-600'
-                onChange= {()=>setSelected(!selected)}
-            />
-</div>
+      <Bar data={generateChartData()} options={options} />
+      <div className='w-56 mx-1 h-1/3 mt-1 shadow-lg rounded-lg p-4 z-[99999999]   border-zinc-800'>
+        <div className='flex-grow z-20  font-mono tracking-widest text-xs mb-5'>hide/show Query types</div>
+        <Input.Checkbox
+          label='select'
+          id='select-toggle'
+          value='select'
+          className='peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 peer-checked:bg-blue-600'
+          onChange={() => setSelected(!selected)}
+        />
+      </div>
     </div>
-    );
+  );
 };
 export default Donuts;
-
--- components/graph/mock-request-list.tsx
+++ components/graph/mock-request-list.tsx
@@ -1,45 +1,45 @@
-import { Request } from "./request";
+import { Request } from './request';
 
- const REQUESTDATA: Request[] =
-  [{
-  "id": "17782UGG",
-  "query": "select",
-   "times_called":200
+const Logs: Request[] = [
+  {
+    id: '17782UGG',
+    query: 'select',
+    times_called: 200,
   },
   {
-    "id": "17782UGG",
-    "query": "select * from dataBase",
-     "times_called":120
-    },
-    {
-      "id": "17782UGG",
-      "query": "select * from user",
-       "times_called":100
-      },
+    id: '17782UGG',
+    query: 'select * from dataBase',
+    times_called: 120,
+  },
   {
-    "id": "17782UNI",
-    "query": "show",
-    "times_called": 180
-    },
-    {
-      "id": "17782UNI",
-      "query": "update",
-      "times_called": 90
-      },
-      {
-        "id": "17782UNI",
-        "query": "delete",
-        "times_called": 60
-        },
-        {
-          "id": "17782UNI",
-          "query": "delete from user",
-          "times_called": 20
-          },
-        {
-          "id": "17782UNI",
-          "query": "Alter",
-          "times_called": 20
-          },
-]
-export default REQUESTDATA;
+    id: '17782UGG',
+    query: 'select * from user',
+    times_called: 100,
+  },
+  {
+    id: '17782UNI',
+    query: 'show',
+    times_called: 180,
+  },
+  {
+    id: '17782UNI',
+    query: 'update',
+    times_called: 90,
+  },
+  {
+    id: '17782UNI',
+    query: 'delete',
+    times_called: 60,
+  },
+  {
+    id: '17782UNI',
+    query: 'delete from user',
+    times_called: 20,
+  },
+  {
+    id: '17782UNI',
+    query: 'Alter',
+    times_called: 20,
+  },
+];
+export default Logs;
